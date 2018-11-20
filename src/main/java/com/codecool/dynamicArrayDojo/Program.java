a7fbd396958eba0dcfac363d58875eee806a191d
Riczard <riczardkiller@gmail.com>
2018-11-21 00:50:09 +0100

    Add flattening list

--- src/main/java/com/codecool/dynamicArrayDojo/Program.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.dynamicArrayDojo;
-
-public class Program {
-
-    public static void main(String[] args) {
-        System.out.println("Do not run me, run the TESTS!");
-    }
-}
